5c6a68d99713ed2476c4c0f14dfe4323a748f92b
stackew <koraega@mail.ru>
2026-09-16 10:53:16 +0300

    Added multi

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@ int main()
 {
 	int a, b;
 	std::cin >> a >> b;
-	std::cout << "Sum:" << a+b << "\n";
+	std::cout << "Sum: " << a+b << "\n";
 	std::cout << "Sub: " << a - b << "\n";
+	std::cout << "Mul: " << a * b << "\n";
 	return 0;
 }
